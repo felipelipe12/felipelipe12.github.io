a9fb0de542a7c2495ab8edfcf797efcce20400f2
Felipe Almeida <felipe.lipe12.fl@outlook.com>
2022-11-21 22:33:03 -0300

    New assets

---
+++ vendor/typed/script.js
@@ -0,0 +1,9 @@
+var options = {
+    strings: ['Web design', 'Front-end', 'UX/UI'],
+    typeSpeed: 100,
+    backDelay: 500,
+    backSpeed: 100,
+    loop: true,
+};
+
+var typed = new Typed('.typed', options);

7654b5f951541f4f29c9b679e6a52dedb35c9316
Felipe Almeida <felipe.lipe12.fl@outlook.com>
2024-10-30 17:23:38 -0300

    Feat: Create a new portfolio with new concepts.

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-var options = {
-    strings: ['Web design', 'Front-end', 'UX/UI'],
-    typeSpeed: 100,
-    backDelay: 500,
-    backSpeed: 100,
-    loop: true,
-};
-
-var typed = new Typed('.typed', options);
